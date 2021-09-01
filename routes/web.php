2cf159beb175ebcc94e3454e52b5d52eee3eba09
vitoxint <vitoxint@gmail.com>
2021-09-01 15:59:08 -0400

    se cambia slug por id en posts vista one

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,11 @@ Route::get('/', function () {
 
 
 
-Route::get('/post/{post}', function($slug){
+Route::get('/post/{post}', function($id){
     return view ('post', [
-        'post'=> Post::find($slug),
+        'post'=> Post::findOrFail($id),
     ]);
-})-> where('post', '[A-Za-z\_-]+');
+});
      
 
 
