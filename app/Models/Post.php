848868da58a9240b0b7213ef3ecc0c2976d884aa
vitoxint <vitoxint@gmail.com>
2021-09-01 15:31:12 -0400

    add_model_post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,49 +1,13 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
 
-    public string $title;
-    public string $resumen;
-    public string $date;
-    public string $slug;
-    public string $body;
-
-    public function __construct( $title, $resumen, $date, $slug, $body ){
-        $this->title = $title;
-        $this->resumen = $resumen;
-        $this->date = $date;
-        $this->slug = $slug;
-        $this->body = $body; 
-    }
-
-    public static function createFromDocument($document){
-        return new self(
-            $document->title,
-            $document->resumen,
-            $document->date,
-            $document->slug,
-            $document->body()
-        );
-    }
-
-    public static function all(){
-     
-      return collect(File::files(resource_path("posts/")))   
-        ->map(fn ($file) => YamlFrontMatter::parseFile($file))
-        ->map(fn ($document) => Post::createFromDocument($document));
-    }
-    public static function find($slug)
-    {
-        $post=static::all()->firstWhere('slug',$slug);
-        return  $post;
-
-        return cache()->remember("post.{$slug}",now()->addDys(1),fn()=>static::all()->firstWhere('slug',$slug));
-
-    }
+    // Traits
+    use HasFactory;
 }
