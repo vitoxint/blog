44ae80ae4f15419e498caa93d51e021c66458156
vitoxint <vitoxint@gmail.com>
2021-09-07 16:47:04 -0300

    cambia url post a  slug to post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,7 +10,7 @@
         @foreach ($posts as $post)
             <article>
                 <h1>
-                    <a href="/post/<?= $post->id?>">
+                    <a href="/post/<?= $post->slug?>">
                         {{ $post->title }}
                     </a>
                 </h1>
