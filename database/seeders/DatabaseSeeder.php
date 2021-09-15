f8dcea2767c1e75a222d5a87b60eaacce55f40e5
vitoxint <vitoxint@gmail.com>
2021-09-15 15:54:53 -0300

    agregardatos con referencia

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use App\Models\Category;
+use App\Models\Post;
 use \App\Models\User;
+use App\Models\Category;
+use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,21 +16,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         User::factory()->create();
 
-         Category::create([
+        User::truncate();
+        Category::truncate();
+
+        $user = User::factory()->create();
+
+        $personal =Category::create([
              'name' => 'Personal',
              'slug' => 'personal'
 
-         ]);
-         Category::create([
+        ]);
+        $work = Category::create([
             'name' => 'Work',
             'slug' => 'work'
 
         ]);
-        Category::create([
+        $hobbies = Category::create([
             'name' => 'Hobbies',
             'slug' => 'hobbies'
+
+        ]);
+
+
+        Post::create([
+            'category_id' => $work->id ,
+            'user_id' => $user->id,
+            'slug' => 'my-first-post',
+            'title' => 'My First Post',
+            'resumen' => 'There any many variations of passages' ,
+            'body'  => "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'"
 
         ]);
 
