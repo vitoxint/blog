0640734ad3a20353745b6f439d95b52f01fb7d64
vitoxint <vitoxint@gmail.com>
2021-09-21 15:36:24 -0300

    mod database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,35 +19,12 @@ class DatabaseSeeder extends Seeder
 
         User::truncate();
         Category::truncate();
-
-        $user = User::factory()->create();
-
-        $personal =Category::create([
-             'name' => 'Personal',
-             'slug' => 'personal'
-
-        ]);
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-
-        ]);
-        $hobbies = Category::create([
-            'name' => 'Hobbies',
-            'slug' => 'hobbies'
-
-        ]);
+        Post::truncate();
 
 
-        Post::create([
-            'category_id' => $work->id ,
-            'user_id' => $user->id,
-            'slug' => 'my-first-post',
-            'title' => 'My First Post',
-            'resumen' => 'There any many variations of passages' ,
-            'body'  => "'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'"
+        Post::factory()->create();
 
-        ]);
+        
 
     }
 }
