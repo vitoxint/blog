b646f030c8bc3cd34d85c5761f2d649614503880
vitoxint <vitoxint@gmail.com>
2021-09-21 15:13:01 -0300

    crear faker posts

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PostFactory extends Factory
@@ -22,6 +24,12 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
+            'category_id' => Category::factory(),
+            'user_id' => User::factory(),
+            'slug' => $this->faker->slug() ,
+            'title' =>  $this->faker->sentence(), 
+            'resumen' => $this->faker->sentence() , 
+            'body' => $this->faker->paragraph()
             //
         ];
     }
