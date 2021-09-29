fff00e3f2fb4a7ffe5cca09cd36746555e9463d4
vitoxint <vitoxint@gmail.com>
2021-09-29 15:49:58 -0300

    images routes final

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
         Latest <span class="text-blue-500">Laravel From Scratch</span> News
     </h1>
 
-    <h2 class="inline-flex mt-2">By Lary Laracore <img src="./images/lary-head.svg"
+    <h2 class="inline-flex mt-2">By Lary Laracore <img src="/images/lary-head.svg"
                                                         alt="Head of Lary the mascot"></h2>
 
     <p class="text-sm mt-14">
@@ -77,7 +77,7 @@
             <article class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
                     <div class="py-6 px-5">
                         <div>
-                            <img src="./images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
+                            <img src="/images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
                         </div>
 
                         <div class="mt-8 flex flex-col justify-between">
@@ -109,7 +109,7 @@
 
                             <footer class="flex justify-between items-center mt-8">
                                 <div class="flex items-center text-sm">
-                                    <img src="./images/lary-avatar.svg" alt="Lary avatar">
+                                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
                                     <div class="ml-3">
                                         <h5 class="font-bold">{{$post->author->name}}</h5>
                                         <h6>Mascot at SEGIC</h6>
