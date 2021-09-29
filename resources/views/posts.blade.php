678cb203725817cabf79ac2951806474a4210a3a
vitoxint <vitoxint@gmail.com>
2021-09-29 15:46:54 -0300

    card listado dinamico tempal2

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -117,7 +117,7 @@
                                 </div>
 
                                 <div>
-                                    <a href="/post/<?= $post->slug?>">
+                                    <a href="/post/<?= $post->slug?>"
                                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
                                     >Read More</a>
                                 </div>
