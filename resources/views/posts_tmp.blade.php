8db3137ed96c2edf526443df2447c004c147d91d
vitoxint <vitoxint@gmail.com>
2021-10-01 15:10:55 -0300

    UTILIZA COMPONENTS EN VES DE BLADE

---
+++ resources/views/posts_tmp.blade.php
@@ -0,0 +1,138 @@
+@extends('layout')
+
+@section('banner')
+    <h1 class="text-4xl">
+        Latest <span class="text-blue-500">Laravel From Scratch</span> News
+    </h1>
+
+    <h2 class="inline-flex mt-2">By Lary Laracore <img src="/images/lary-head.svg"
+                                                        alt="Head of Lary the mascot"></h2>
+
+    <p class="text-sm mt-14">
+        Another year. Another update. We're refreshing the popular Laravel series with new content.
+        I'm going to keep you guys up to speed with what's going on!
+    </p>
+
+    <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
+        <!--  Category -->
+        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
+            <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
+                <option value="category" disabled selected>Category
+                </option>
+                <option value="personal">Personal</option>
+                <option value="business">Business</option>
+            </select>
+
+            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
+                    height="22" viewBox="0 0 22 22">
+                <g fill="none" fill-rule="evenodd">
+                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
+                    </path>
+                    <path fill="#222"
+                            d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                </g>
+            </svg>
+        </div>
+
+        <!-- Other Filters -->
+        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
+            <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
+                <option value="category" disabled selected>Other Filters
+                </option>
+                <option value="foo">Foo
+                </option>
+                <option value="bar">Bar
+                </option>
+            </select>
+
+            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
+                    height="22" viewBox="0 0 22 22">
+                <g fill="none" fill-rule="evenodd">
+                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
+                    </path>
+                    <path fill="#222"
+                            d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                </g>
+            </svg>
+        </div>
+
+        <!-- Search -->
+        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
+            <form method="GET" action="#">
+                <input type="text" name="search" placeholder="Find something"
+                        class="bg-transparent placeholder-black font-semibold text-sm">
+            </form>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+
+<div class="lg:grid lg:grid-cols-3">
+
+    @if (count($posts) > 0)
+        @foreach ($posts as $post)
+
+           
+            <article class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+                    <div class="py-6 px-5">
+                        <div>
+                            <img src="/images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
+                        </div>
+
+                        <div class="mt-8 flex flex-col justify-between">
+                            <header>
+                                <div class="space-x-2">
+                                    <a href="/category/{{$post->category->slug}}"
+                                       class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
+                                       style="font-size: 10px">{{$post->category->name}}
+                                    </a>                                   
+                                </div>
+
+                                <div class="mt-4">
+                                    <h1 class="text-3xl">
+                                        {{ $post->title }}
+                                    </h1>
+
+                                    <span class="mt-2 block text-gray-400 text-xs">
+                                        Published <time>1 day ago</time>
+                                    </span>
+                                </div>
+                            </header>
+
+                            <div class="text-sm mt-4">
+                                <p>
+                                    {{ $post->resumen }} 
+                                </p>
+
+                            </div>
+
+                            <footer class="flex justify-between items-center mt-8">
+                                <div class="flex items-center text-sm">
+                                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
+                                    <div class="ml-3">
+                                        <h5 class="font-bold"> <a href="/author/{{$post->user_id}}">  {{$post->author->name}}  </a></h5>
+                                        <h6>Mascot at SEGIC</h6>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <a href="/post/<?= $post->slug?>"
+                                       class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
+                                    >Read More</a>
+                                </div>
+                            </footer>
+                        </div>
+                    </div>
+
+            </article>
+
+
+        @endforeach
+    @else
+        I don't have any posts!
+    @endif 
+
+</div>
+
+@endsection
