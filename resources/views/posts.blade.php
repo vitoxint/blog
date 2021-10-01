ea1027ddfba10ddf36ebe4554335baebc5b72af1
vitoxint <vitoxint@gmail.com>
2021-10-01 15:42:03 -0300

    se saca el contenido del layout

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,22 @@
 <x-layout >
-    Hola Segic , Estoy utilizando la variable slot !!
-    <h1> Renderizar dentro del componente</h1>
+@include('_posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        
+
+        <div class="lg:grid lg:grid-cols-2">
+            
+            <x-post-card />
+            <x-post-card />
+            
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+            
+        </div>
+    </main>
 </x-layout>
