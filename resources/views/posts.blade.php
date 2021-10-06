74c5e2f2ed1f72e8ec1244ab72f7236744eec747
vitoxint <vitoxint@gmail.com>
2021-10-06 17:48:48 -0300

    clase 64 se elimina comentarios

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,7 @@
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         
-        <x-post-main :post="$posts[0]">
+        <x-post-main :post="$posts[0]" />
 
         <div class="lg:grid lg:grid-cols-2">
             
