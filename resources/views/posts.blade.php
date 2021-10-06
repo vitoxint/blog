ac1e538982aec3017d9d6c97ad5aa4b943bb3155
vitoxint <vitoxint@gmail.com>
2021-10-06 17:57:00 -0300

    clase 65 ejemplo post card y main

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,25 +1,14 @@
-<x-layout >
+<x-layout>        
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        
         <x-post-main :post="$posts[0]" />
 
-        <div class="lg:grid lg:grid-cols-2">
-            
-            <x-post-card />
-            <x-post-card />
-     
+        <div class="lg:grid lg:grid-cols-6">
+            @foreach ($posts->skip(1) as $post)            
+                <x-post-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}" />
+            @endforeach
         </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-            
-        </div>
-
     </main>
-
+    
 </x-layout>
