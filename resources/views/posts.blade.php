43a740977cbc448d0c31371f1340c876707aa061
vitoxint <vitoxint@gmail.com>
2021-10-06 17:39:02 -0300

    clase 64 se elimina comentarios

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,15 @@
 <x-layout >
-@include('_posts-header')
+    @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         
-        <!-- <x-post-main :post="$posts[0]"> -->
+        <x-post-main :post="$posts[0]">
 
         <div class="lg:grid lg:grid-cols-2">
             
             <x-post-card />
             <x-post-card />
-
-            
+     
         </div>
 
         <div class="lg:grid lg:grid-cols-3">
@@ -20,5 +19,7 @@
             <x-post-card />
             
         </div>
+
     </main>
+
 </x-layout>
