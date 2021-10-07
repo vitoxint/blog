7c33b9de99c455e35bcd8f8d72ebc9d1bdc09164
vitoxint <vitoxint@gmail.com>
2021-10-07 15:06:29 -0300

    category button component intento 4

--- resources/views/components/post-main.blade.php
+++ resources/views/components/post-main.blade.php
@@ -1,35 +1,38 @@
 @props(['post'])
 <article
-    {{ $attributes->merge(['class' =>'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl']) }}>
-    <div class="py-6 px-5">
-        <div>
-            <img src="/images/illustration-5.png" alt="Blog Post illustration" class="rounded-xl">
+    class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+    <div class="py-6 px-5 lg:flex">
+        <div class="flex-1 lg:mr-8">
+            <img src="/images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
         </div>
 
-        <div class="mt-8 flex flex-col justify-between">
-            <header>
+        <div class="flex-1 flex flex-col justify-between">
+            <header class="mt-8 lg:mt-0">
                 <div class="space-x-2">
-                    <!-- <a href="/category/{{$post->category->slug}}"
+                  <!--   <a href="#"
                         class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
                         style="font-size: 10px">
                         {{ $post->category->name}}
                     </a> -->
                     <x-category-button :post="$post" />
+
                 </div>
 
                 <div class="mt-4">
                     <h1 class="text-3xl">
-                        This is a big title and it will look great on two or even three lines. Wooohoo!
+                        {{ $post->title }}
                     </h1>
 
                     <span class="mt-2 block text-gray-400 text-xs">
-                    Published <time>{{ $post->created_at->diffForHumans() }}</time>
-                    </span>
+                            Published <time>{{ $post->created_at->diffForHumans() }}</time>
+                        </span>
                 </div>
             </header>
 
-            <div class="text-sm mt-4">
-                <p>{{ $post->resumen }}</p>
+            <div class="text-sm mt-2">
+                <p>
+                    {{ $post->resumen }}
+                </p>
             </div>
 
             <footer class="flex justify-between items-center mt-8">
@@ -41,11 +44,10 @@
                     </div>
                 </div>
 
-                <div>
+                <div class="hidden lg:block">
                     <a href="/post/{{ $post->slug }}"
-                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">
-                        Read More
-                    </a>
+                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
+                    >Read More</a>
                 </div>
             </footer>
         </div>
