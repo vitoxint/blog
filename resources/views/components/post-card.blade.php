4f606f8c84dee5351e316fd2fbd6af76aaad4799
vitoxint <vitoxint@gmail.com>
2021-10-07 12:20:12 -0300

    clase 65 ejemplo post detalle

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -9,7 +9,7 @@
         <div class="mt-8 flex flex-col justify-between">
             <header>
                 <div class="space-x-2">
-                    <a href="#"
+                    <a href="/category/{{$post->category->slug}}"
                         class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
                         style="font-size: 10px">
                         {{ $post->category->name}}
@@ -18,7 +18,7 @@
 
                 <div class="mt-4">
                     <h1 class="text-3xl">
-                        This is a big title and it will look great on two or even three lines. Wooohoo!
+                        {{$post->title}}
                     </h1>
 
                     <span class="mt-2 block text-gray-400 text-xs">
@@ -41,7 +41,7 @@
                 </div>
 
                 <div>
-                    <<a href="/post/{{ $post->slug }}"
+                    <a href="/post/{{ $post->slug }}"
                         class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">
                         Read More
                     </a>
