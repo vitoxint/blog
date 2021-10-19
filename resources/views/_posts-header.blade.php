960d64899225113fd5c28305473b0b53fc32c12f
vitoxint <vitoxint@gmail.com>
2021-10-19 16:10:10 -0300

    select link use alpinejs 15

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -31,7 +31,7 @@
                 <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50 text-left px-3 " style="display:none">
                     @foreach( $categories as $category)
                     <a href="/category/{{$category->slug}}" 
-                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white {{isset( $currentCategory) && $currentCategory->is( $category) ? bg-blue-500 text-white : ''}}">
+                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white {{isset( $currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : ''}}">
                         {{ ucwords($category->name) }}
                     </a>
                     @endforeach
