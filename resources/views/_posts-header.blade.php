60ed4ae85d596ac5d1108e4344cfc7439f66425a
vitoxint <vitoxint@gmail.com>
2021-10-19 15:37:34 -0300

    select link use alpinejs 8

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -17,11 +17,20 @@
             <div x-data="{ show : false}">
                 <button @click="show = !show" class="py-2 pl-3 pr-9 text-sm font-semibold inline-flex">
                     Category
+                    <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
+                            height="22" viewBox="0 0 22 22">
+                        <g fill="none" fill-rule="evenodd">
+                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
+                            </path>
+                            <path fill="#222"
+                                    d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                        </g>
+                    </svg>
                 </button>
-                <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl">
-                    <a href="#" class="block text-left px-3 text-sm">One</a>
-                    <a href="#" class="block text-left px-3 text-sm">Two</a>
-                    <a href="#" class="block text-left px-3 text-sm">Three</a>
+                <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50 text-left px-3">
+                    <a href="#" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white">One</a>
+                    <a href="#" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white">Two</a>
+                    <a href="#" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white">Three</a>
 
                 </div>
                 
