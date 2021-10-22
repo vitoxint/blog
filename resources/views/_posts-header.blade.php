912b5cd303e8846533bb9774b6271717c7b27785
vitoxint <vitoxint@gmail.com>
2021-10-22 16:13:56 -0300

    select link use alpinejs 21

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -34,12 +34,13 @@
                      All
                 </x-dropdown-item>
 
-                @foreach( $categories as $category)
-                    <x-dropdown-item :href="/category/{{$category->slug}}" :actived="(isset( $currentCategory ) && $currentCategory->is($category)) ? true : false">
-                        {{ ucwords($category->name) }}
+                @foreach ($categories as $category)
+                    <x-dropdown-item 
+                        href="/category/{{ $category->slug }}" 
+                        :actived="isset($currentCategory) && $currentCategory->is($category)"
+                        > {{ ucwords($category->name) }}
                     </x-dropdown-item>
                 @endforeach
-
 
             </x-dropdown>
             
