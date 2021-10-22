9e8fbed45666432025c6cff1128cf938c5ba1780
vitoxint <vitoxint@gmail.com>
2021-10-22 15:23:38 -0300

    select link use alpinejs 18

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -35,6 +35,8 @@
 
                 </x-slot>
 
+                <a href="/" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white">All</a>
+
                 @foreach( $categories as $category)
                     <a href="/category/{{$category->slug}}" 
                         class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white {{isset( $currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : ''}}">
