0fb6f722428495bd57868376063b732c48e0fbea
vitoxint <vitoxint@gmail.com>
2021-10-22 17:11:23 -0300

    select link use alpinejs 24

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -1,6 +1,6 @@
 @props(['name'])
 
-<svg {{ $attributes(['class' => '"transform -rotate-90 '])}} class="absolute pointer-events-none" style="right: 12px;" width="22"
+<svg {{ $attributes(['class' => 'transform -rotate-90'])}} class="absolute pointer-events-none" style="right: 12px;" width="22"
         height="22" viewBox="0 0 22 22">
     <g fill="none" fill-rule="evenodd">
         <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
