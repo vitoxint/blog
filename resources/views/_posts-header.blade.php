9154443f23465a75b270bea5944bd2f05041a626
vitoxint <vitoxint@gmail.com>
2021-10-25 12:27:51 -0300

    select link use alpinejs 26

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -19,7 +19,7 @@
 
             <x-dropdown>
                 <x-slot name="trigger">
-                    <button @click="show = !show" class="py-2 pl-3 pr-9 text-sm font-semibold  lg:w-32 w-full flex lg:inline-flex">
+                    <button class="py-2 pl-3 pr-9 text-sm font-semibold  lg:w-32 w-full flex lg:inline-flex">
                         {{ isset( $currentCategory ) ? ucwords($currentCategory->name) : "Categories"}}
                         
                         <x-icon name="down-arrow" class="absolute pointer-events-none" style="right:12px " />
