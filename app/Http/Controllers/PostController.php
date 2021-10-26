b84a9222d5d645ec40d4406369b28f7674339ea3
vitoxint <vitoxint@gmail.com>
2021-10-26 15:45:14 -0300

    se implementa controller a la ruta principal 4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,7 +21,7 @@ class PostController extends Controller
     }
 
 
-    public function show($post){
+    public function show( Post $post){
 
         return view ('post', [
             'post'=> $post ,
